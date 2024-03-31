9257c3b06bb23e1b0c2815a2f3b8a7ff7a432c38
Harmfullllll <tanvirjoy114@gmail.com>
2024-03-31 16:12:20 +0600

    added multer middleware

---
+++ src/models/user.model.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      min: 6,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password"))
+    this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_EXPIRY,
+    }
+  );
+};
+export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+const { LOADIPHLPAPI } = require("dns");
+const fs = require("fs");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFile = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    return await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+  } catch (err) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
